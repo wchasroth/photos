552bbf8fc7a476949de47f24ff1e98ce167a0d1b
Charles Roth <croth@thedance.net>
2024-11-29 16:17:47 -0500

    Set up initial photos github repo

---
+++ htdocs/index.php
@@ -0,0 +1,57 @@
+<?php
+   use charlesroth_net\Utils\SmartyPage;
+   use charlesroth_net\Utils\Env;
+
+   require_once('../vendor/autoload.php');
+
+   $env = Env::getEnv();
+   $page = getGet('page', "Albums");
+   $dir = $env['path'] . $page;
+
+   $breadcrumb = array();
+   $prevUri = "";
+   foreach (explode("/", $page) as $item) {
+      $itemRecord = array("uri" => "index.php?page=$prevUri$item", "label" => substringBefore($item, ".dir"));
+      $breadcrumb[] = $itemRecord;
+      $prevUri = "$prevUri$item/";
+   }
+
+   $files = scandir($dir);
+   $fileDict = array();
+   foreach ($files as $file)  addFileEntry($fileDict, $file);
+
+   $smarty = new SmartyPage();
+   $smarty->assign("fileDict", $fileDict);
+   $smarty->assign("breadcrumb", $breadcrumb);
+   $smarty->assign("page", $page);
+   $smarty->assign("dir", $dir);
+   $smarty->display("index.tpl");
+
+   function getGet(string $name, $default): string {
+      if (empty($name))  return "";
+      return isset($_GET[$name])  ? trim($_GET[$name])  : $default;
+   }
+
+   function addFileEntry (&$fileDict, $fileName): void {
+      if (str_starts_with($fileName, "."))  return;
+      if (str_starts_with($fileName, "_"))  return;
+
+      $basename = substringBefore($fileName, ".");
+      if (! array_key_exists($basename, $fileDict)) {
+         $fileRecord = array("dir" => "", "img" => "", "thu" => "", "txt" => "");
+         $fileDict[$basename] = $fileRecord;
+      }
+
+      $fileRecord = &$fileDict[$basename];
+      if     (str_ends_with($fileName, "dir" ))  $fileRecord['dir'] = $fileName;
+      elseif (str_contains($fileName, ".img."))  $fileRecord['img'] = $fileName;
+      elseif (str_contains($fileName, ".thu."))  $fileRecord['thu'] = $fileName;
+      elseif (str_contains($fileName, ".txt."))  $fileRecord['txt'] = $fileName; // should be contents of file!
+   }
+
+   function substringBefore (string $text, string $delimiter): string {
+      $pos = strpos($text, $delimiter);
+      if ($pos === false)  return $text;
+      return substr($text, 0, $pos);
+   }
+
---
+++ templates/index.tpl
@@ -0,0 +1,54 @@
+{nocache}
+<html>
+<head>
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto">
+   <style type="text/css">
+      body, td, div { font-family: 'Roboto', sans-serif; }
+   </style>
+</head>
+<body>
+
+{assign var="separator" value=""}
+{foreach $breadcrumb as $crumb}
+   {$separator} <a href="{$crumb.uri}">{$crumb.label}</a>
+   {assign var="separator" value="=&gt;"}
+{/foreach}
+
+<table border='0'>
+<tr valign='top'>
+
+{foreach $fileDict as $record}
+   {if $record@iteration is div by 4}
+      </tr><tr valign='top'>
+   {/if}
+
+   <td style="position: relative; padding: .5em;">
+      {if $record.dir != ""}
+         <a href="index.php?page={$page}/{$record.dir}">
+      {else}
+         <a href="{$page}/{$record.img}">
+      {/if}
+   
+      {if $record.thu != ""}
+         <img src='{$page}/{$record.thu}'><br/>
+      {else}
+         <img src='Albums/_Icons/nothumb.png'><br/>
+      {/if}
+      </a>
+      <br/>
+   
+      <div style="position: absolute; bottom: 0; text-align: center; width: 100%;">
+         {if $record.dir != ""}
+            {$record.dir}<br/>
+         {else}
+            {$record.img}</br>
+         {/if}
+      </div>
+   </td>
+{/foreach}
+</tr>
+</table>
+
+</body>
+</html>
+{/nocache}
